a1f43753b1360fe13aad288866cd8fa53f2e3056
Michael Zbeetnoff <miczbeetnoff@hotmail.com>
2021-03-07 09:41:36 -0800

    Initial commit
    Modify Contacts component to use MUIDataTable component

--- brewcraft/src/pages/Contacts/components/contacts-table.js
+++ brewcraft/src/pages/Contacts/components/contacts-table.js
@@ -1,69 +1,85 @@
-import React,{useCallback} from "react";
-import { MDBDataTable } from "mdbreact";
-import {map} from "lodash";
-import {
-  Button
-} from 'reactstrap';
+import React, { useState, useCallback } from "react";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import MUIDataTable, { TableFilterList, TableToolbarSelect } from "mui-datatables";
+import Chip from '@material-ui/core/Chip';
+import { map } from "lodash";
 
 export default function ContactsTable({data, editCompany, editContact, deleteContact}) {
-  const tabledata = [
-    {
-      label: "Name",
-      field: "contactName",
-      sort: "asc",
-      width: 200,
-      onClick: editContact
-    },
-    {
-      label: "Company",
-      field: "companyName",
-      sort: "asc",
-      width: 200,
-      onClick: editCompany
-    },
-    {
-      label: "Phone",
-      field: "phoneNumber",
-      sort: "asc",
-      width: 100,
-    },
-    {
-      label: "Email",
-      field: "email",
-      sort: "asc",
-      width: 100,
-    },
-    {
-      label: "",
-      field: "edit"
-    },{
-      label: "",
-      field: "delete"
+  const columns = [{
+    name: "First Name",
+    options: {
+      filter: true,
+      sort: true
     }
-  ];
+  }, {
+    name: "Last Name",
+    options: {
+      filter: true,
+      sort: true
+    }
+  }, {
+    name: "Company",
+    options: {
+      filter: true,
+      sort: true
+    }
+  }, {
+    name: "Position",
+    options: {
+      filter: true,
+      sort: false,
+      display: false
+    }
+  }, {
+    name: "Phone",
+    options: {
+      filter: false,
+      sort: false
+    }
+  }, {
+    name: "Email",
+    options: {
+      filter: false,
+      sort: false
+    }
+  }];
   const rowEvent = useCallback(() => {
     return map(data, (row) => {
-      return {
-        ...row,
-        contactName: row.firstName,
-        companyName: row.cName,
-        edit: (
-          <Button onClick={() => editContact(row.id)}>Edit</Button>
-        ),
-        delete: (
-          <Button onClick={() => deleteContact(row.id)}>Delete</Button>
-        )
-      };
+      return [
+        row.firstName,
+        row.lastName,
+        row.supplier.name,
+        row.position,
+        row.phoneNumber,
+        row.email
+      ];
     });
   }, [data]);
-  return (
-    <MDBDataTable
-      responsive
-      bordered
-      data={{
-        columns: tabledata,
-        rows: rowEvent()
-      }}
-    />
-  );
+
+  const theme = () => {
+    return createMuiTheme({
+      overrides: {
+        MUIDataTable: {
+          paper: {
+            boxShadow: "none"
+          },
+        },
+      }
+    });
+  }
+
+  return(
+    <MuiThemeProvider theme={theme()}>
+      <MUIDataTable
+        columns={columns}
+        data={rowEvent()}
+        options={{
+          filter: true,
+          filterType: "checkbox",
+          search: true,
+          // tableBodyHeight: "400px"
+        }}
+      />
+    </MuiThemeProvider>
+  )
 }
