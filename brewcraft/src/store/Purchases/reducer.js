b096f72f9b7786f843c7770a70942ae7adde58c5
Michael Zbeetnoff <miczbeetnoff@hotmail.com>
2021-03-15 20:49:03 -0700

    update purchase invoices store

--- brewcraft/src/store/Purchases/reducer.js
+++ brewcraft/src/store/Purchases/reducer.js
@@ -1,11 +1,26 @@
 import { MOCK_TABLE_DATA } from "../../component/Invoice/mock";
+import {
+  FETCH_PURCHASES_REQUEST,
+  FETCH_PURCHASE_REQUEST
+} from "./actionTypes";
 
 const initialState = {
   invoices: MOCK_TABLE_DATA
 };
 
-const purchases = (state = initialState, action) => {
-  switch (action.type) {
+const purchases = (state = initialState, { type, payload }) => {
+
+  switch (type) {
+    case FETCH_PURCHASES_REQUEST:
+      return {
+        ...state,
+        purchases: payload
+      };
+    case FETCH_PURCHASE_REQUEST:
+      return {
+        ...state,
+        purchase: payload
+      };
     default:
       state = { ...state };
       break;
