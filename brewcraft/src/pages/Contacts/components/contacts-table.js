b616b5ccfed15205d84ae90ca120107b6bb3d01a
Sachitha kariyawasam <klgsachitha@gmail.com>
2021-03-21 00:18:39 +0530

    implement the reusable mui datatable and uses it for the suppliers table (#62)
    * implement the reusable mui datatable and uses it for the suppliers table
    
    * modify companies table
    
    Co-authored-by: sachitha <sachitha@meetrix.io>

--- brewcraft/src/pages/Contacts/components/contacts-table.js
+++ brewcraft/src/pages/Contacts/components/contacts-table.js
@@ -1,14 +1,9 @@
-import React, { useState, useCallback } from "react";
-import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
-import {IconButton, Tooltip,Typography,Button,TextField,TableFooter, TableCell, TableRow} from '@material-ui/core';
-import {Add, Edit,Delete, Refresh} from '@material-ui/icons';
-import MUIDataTable, { TableFilterList, TableToolbarSelect, } from "mui-datatables";
-import Chip from '@material-ui/core/Chip';
+import React, { useCallback } from "react";
+import {Button} from '@material-ui/core';
 import { map } from "lodash";
+import DataTable from '../../../component/Tables/tables-mui-datatable';
 
-export default function ContactsTable({data, editContact, deleteContact, addContact,refreshTable}) {
-
-  const[searchText,setSearchText]=useState("");
+export default function ContactsTable({data, editContact, deleteContact, addContact, refreshTable}) {
 
   const columns = [{
     label: "First Name",
@@ -55,6 +50,15 @@ export default function ContactsTable({data, editContact, deleteContact, addCont
     }
   }];
 
+  const onDelete=(rowsSelected)=>{
+    const idsToDelete = rowsSelected.data.map(d => data[d.dataIndex].id); // array of all ids to to be deleted
+    idsToDelete.map((id)=>deleteContact(id));
+  }
+
+  const onEdit=(rowsSelected)=>{
+    const idToEdit = data[rowsSelected?.data[0].dataIndex].id;
+    editContact(idToEdit);
+  }
 
   const rowEvent = useCallback(() => {
     return map(data, (row) => {
@@ -73,170 +77,15 @@ export default function ContactsTable({data, editContact, deleteContact, addCont
 
   }, [data]);
 
-  const theme = () => {
-    return createMuiTheme({
-      overrides: {
-        MUIDataTable: {
-          paper: {
-            boxShadow: "none"
-          },
-        },
-        MUIDataTableToolbar: {
-          root: {
-            minHeight:'40px'
-          },
-          actions: {
-            display: "flex",
-            flex: "initial",
-            // move all icons to the right
-            "& > span, & > button, & input": {
-              order: 0
-            },
-            // // target the custom toolbar icon
-            "& > span:last-child, & > button:last-child": {
-              order: 99
-            },
-            // // target any icon
-            "& > button:nth-child(5), & > button:nth-child(5)": {
-              order: -1
-            }
-          },
-          icon: {
-            '&:hover': {
-                 color: "#7a6fbe"
-             }
-         },
-        },
-        MUIDataTableFooter:{
-          root:{
-            marginTop:'25px'
-          },
-        },
-        MuiTableRow: {
-          root: {
-              "&:last-child td": {
-                  borderBottom: 0,
-              },
-          }
-        },
-        MuiIconButton: {
-          root: {
-            '&:hover': {
-              color: "#7a6fbe"
-          },
-          borderRadius:'0%'
-          }
-        },
-        MuiCheckbox: {
-          root: {
-            "&$checked$checked": {
-                color:"#7a6fbe"
-            }
-          }
-        },
-        MuiOutlinedInput: {
-          root: {
-            "&:hover $notchedOutline": {
-              borderColor: "#7a6fbe"
-            },
-            "&$focused $notchedOutline": {
-              borderColor: "#7a6fbe"
-            },
-            "&&& $input": {
-              padding: "6px"
-            }
-          }
-        },
-        MuiFormControl:{
-          root:{
-            padding:8
-          }
-        },
-        MuiInputBase: {
-          input: {
-            height:'1.3em',
-            fontSize: '0.71094rem',
-          },
-        }
-      },
-      typography: {
-        "fontFamily": 'Poppins',
-      },
-    }
-    );
-  };
-
-  const deleteButtonOnClick=(rowsDeleted)=>{
-    const idsToDelete = rowsDeleted.data.map(d => data[d.dataIndex].id); // array of all ids to to be deleted
-    idsToDelete.map((id)=>deleteContact(id));
-  }
-
-  const editButtonOnClick=(rowsSelected)=>{
-    const idToEdit = data[rowsSelected?.data[0].dataIndex].id;
-    editContact(idToEdit);
-  }
-
-  const refreshButtonOnClick=()=>{
-   setSearchText("");
-   refreshTable();
-  }
-
-  const customToolbarButton = () => (
-    <>
-      <Tooltip disableFocusListener title="Add Contact">
-        <IconButton onClick={() =>addContact()}>
-          <Add  />
-      </IconButton>
-      </Tooltip>
-      <Tooltip disableFocusListener title="Refresh">
-        <IconButton onClick={() =>refreshButtonOnClick()}>
-          <Refresh  />
-        </IconButton>
-      </Tooltip>
-      <TextField
-          id="contacts-search"
-          placeholder="Search"
-          type="search"
-          variant="outlined"
-          onChange={(e)=>setSearchText(e.target.value)}
-        value={searchText}/>
-      </>
-  );
-
-  const customToolbarSelectButton = (rowsSelected) => {
-    const isShowEditButton=rowsSelected?.data?.length>1 ? false: true;
-
   return(
-    <div>
-  { isShowEditButton && <Tooltip disableFocusListener title="Edit Contact">
-      <IconButton onClick={() =>editButtonOnClick(rowsSelected)}>
-        <Edit  />
-      </IconButton>
-    </Tooltip>}
-        <Tooltip disableFocusListener title="Delete Contact">
-        <IconButton onClick={() =>deleteButtonOnClick(rowsSelected)}>
-          <Delete  />
-        </IconButton>
-      </Tooltip>
-      </div>
-  );
-}
-
-  return(
-    <MuiThemeProvider theme={theme()}>
-      <MUIDataTable
+      <DataTable
         columns={columns}
         data={rowEvent()}
-        options={{
-          filter: true,
-          filterType: "checkbox",
-          search: false,
-          searchText,
-          customToolbar:customToolbarButton,
-          customToolbarSelect:customToolbarSelectButton
-          // tableBodyHeight: "400px"
-        }}
+        tableName="Supplier"
+        editAction={onEdit}
+        deleteAction={onDelete}
+        addAction={addContact}
+        refreshAction={refreshTable}
       />
-    </MuiThemeProvider>
   )
 }
