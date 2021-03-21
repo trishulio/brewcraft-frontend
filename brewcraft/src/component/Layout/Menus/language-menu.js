cbf48b83dfee51424665e399af9b2019b439bc59
Michael Zbeetnoff <miczbeetnoff@hotmail.com>
2021-03-20 18:30:23 -0700

    udpate canada flag and enable language select dialog

--- brewcraft/src/component/Layout/Menus/language-menu.js
+++ brewcraft/src/component/Layout/Menus/language-menu.js
@@ -28,16 +28,17 @@ class LanguageMenu extends Component {
         return (
             <React.Fragment>
                    <Dropdown isOpen={this.state.menu} toggle={this.toggle} id="languagemenu" className={"d-none ml-2 " + this.props.class}>
-                        <DropdownToggle tag="button" className="btn header-item waves-effect" disabled={true}>
+                        <DropdownToggle tag="button" className="btn header-item waves-effect" disabled={false}>
                             <img className="mr-2" src={canFlag} alt="Header Language" height="16"/> English <span className="mdi mdi-chevron-down"></span>
                         </DropdownToggle>
                         <DropdownMenu right>
-                            <DropdownItem tag="a" href="#" className="notify-item"><img src={usFlag} alt="user" className="mr-1" height="12"/> English <span className="align-middle"></span></DropdownItem>
+                        <DropdownItem tag="a" href="#" className="notify-item"><img src={canFlag} alt="user" className="mr-1" height="12"/> <span className="align-middle"> English </span></DropdownItem>
+                            {/* <DropdownItem tag="a" href="#" className="notify-item"><img src={usFlag} alt="user" className="mr-1" height="12"/> English <span className="align-middle"></span></DropdownItem>
                             <DropdownItem tag="a" href="#" className="notify-item"><img src={germanyFlag} alt="user" className="mr-1" height="12"/> <span className="align-middle"> German </span></DropdownItem>
                             <DropdownItem tag="a" href="#" className="notify-item"><img src={italyFlag} alt="user" className="mr-1" height="12"/> <span className="align-middle"> Italian </span></DropdownItem>
                             <DropdownItem tag="a" href="#" className="notify-item"><img src={frenchFlag} alt="user" className="mr-1" height="12"/> <span className="align-middle"> French </span></DropdownItem>
                             <DropdownItem tag="a" href="#" className="notify-item"><img src={spainFlag} alt="user" className="mr-1" height="12"/> <span className="align-middle"> Spanish </span></DropdownItem>
-                            <DropdownItem tag="a" href="#" className="notify-item"><img src={russiaFlag} alt="user" className="mr-1" height="12"/> <span className="align-middle"> Russian </span></DropdownItem>
+                            <DropdownItem tag="a" href="#" className="notify-item"><img src={russiaFlag} alt="user" className="mr-1" height="12"/> <span className="align-middle"> Russian </span></DropdownItem> */}
                         </DropdownMenu>
                     </Dropdown>
             </React.Fragment>
