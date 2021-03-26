7073e9c2671562d508fd386d9e7f3dac208a0d47
vedanu <vedanuj@yahoo.com>
2021-03-26 08:45:08 +0530

    add bootstrap table

--- brewcraft/src/pages/Facility/facility.jsx
+++ brewcraft/src/pages/Facility/facility.jsx
@@ -10,59 +10,57 @@ import {
   fetchFacilities,
   createFacility,
   deleteFacilities,
-  updateFacility
+  updateFacility,
 } from "../../store/Equipment/actions";
+import IconsTable from "../../component/IconsTable";
+import BootstrapTable from "react-bootstrap-table-next";
+import filterFactory, { textFilter } from "react-bootstrap-table2-filter";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import ToolkitProvider, {
+  ColumnToggle,
+  Search,
+} from "react-bootstrap-table2-toolkit";
 import { MDBCard, MDBCardBody, MDBCardHeader, MDBBtn } from "mdbreact";
 
 export default function Facility() {
   const [isOpen, setIsOpen] = useState(false);
   const [editForm, setEditForm] = useState({ edit: false, formData: null });
-  const [tableData, setTableData] = useState({
-    columns: [
+  const [tableData, setTableData] = useState([     
       {
-        label: "",
-        field: "check",
-        // 'sort': 'asc'
+        text: "Name",
+        dataField: "name",
+        sort: true
       },
       {
-        label: "Name",
-        field: "name",
-        sort: "asc",
-        width: 150,
+        text: "Phone Number",
+        dataField: "phoneNumber",
+        sort: true
       },
       {
-        label: "Phone Number",
-        field: "phoneNumber",
-        sort: "asc",
-        width: 270,
+        text: "Address Line 1",
+        dataField: "addressLine1",
+        sort: true
       },
       {
-        label: "Address Line 1",
-        field: "addressLine1",
-        sort: "asc",
-        width: 200,
+        text: "Address Line 2",
+        dataField: "addressLine2",
+        sort: true
       },
       {
-        label: "Address Line 2",
-        field: "addressLine2",
-        sort: "asc",
-        width: 200,
+        text: "City",
+        dataField: "city",
+        sort: true
       },
       {
-        label: "City",
-        field: "city",
-        sort: "asc",
-        width: 200,
+        text: "Country",
+        dataField: "country",
+        sort: true
       },
-      {
-        label: "Country",
-        field: "country",
-        sort: "asc",
-        width: 200,
-      },
-    ],
-    rows: [],
-  });
+    ]  
+    
+  );
+  const { ToggleList } = ColumnToggle;
+  const { SearchBar } = Search;
   const [selectRows, setSelectRows] = useState([]);
   const [deleteIsopen, setDeleteIsopen] = useState(false);
   const dispatch = useDispatch();
@@ -93,51 +91,29 @@ export default function Facility() {
     storages: [],
   };
   /**
-   *
-   * @param {Event} event
-   * @param {Number} itemID
-   */
-  const rowSelectionFn = (event, itemID) => {
-    if (event.target.checked) {
-      const current = [...selectRows];
-      current.push(itemID);
-      setSelectRows(current);
-    } else {
-      const current = selectRows.filter((rowValue) => rowValue != itemID);
-      setSelectRows(current);
-    }
-  };
-  /**
    * @description row iteration
    */
   const tableRows = map(facilities, (currentList) => {
     return {
-      ...omit({ ...currentList, ...currentList.address }, [
+      ...omit({ ...currentList, ...omit(currentList.address,'id') }, [
         "address",
         "equipment",
         "storages",
       ]),
-      check: (
-        <Input
-          type="checkbox"
-          key={`rows_${currentList.id}`}
-          onClick={(e) => rowSelectionFn(e, currentList.id)}
-        />
-      ),
     };
   });
 
   const dialogCloseFn = () => {
     setIsOpen(false);
-    setEditForm({edit: false, formData: null})
+    setEditForm({ edit: false, formData: null });
   };
   const dialogOpenFn = () => {
     setIsOpen(true);
   };
 
   const createFacilities = (e, model) => {
-    if(editForm.edit){
-      const Model = { ...model, id:get(selectRows,'[0]') };
+    if (editForm.edit) {
+      const Model = { ...model, id: get(selectRows, "[0]") };
       dispatch(
         updateFacility({
           formData: Model,
@@ -146,9 +122,8 @@ export default function Facility() {
           },
         })
       );
-
-    }else{
-      // 
+    } else {
+      //
       const Model = { ...FormModal, ...model };
       dispatch(
         createFacility({
@@ -162,12 +137,14 @@ export default function Facility() {
   };
   const dialogOpenEditFn = () => {
     // selectRows
-    const IndexVlaue = findIndex(facilities, (facilitie)=>facilitie.id===selectRows[0])
-      if( IndexVlaue != -1){
-        setEditForm({edit: true, formData: {...facilities[IndexVlaue]}});
-        setIsOpen(true);
-      }  
-
+    const IndexVlaue = findIndex(
+      facilities,
+      (facilitie) => facilitie.id === selectRows[0]
+    );
+    if (IndexVlaue != -1) {
+      setEditForm({ edit: true, formData: { ...facilities[IndexVlaue] } });
+      setIsOpen(true);
+    }
   };
   /**
    * @delete Fun
@@ -179,52 +156,109 @@ export default function Facility() {
   };
   const dialogOpenDeleteCloseFn = () => setDeleteIsopen(false);
   const deleteConfirmFn = () => {
-    dispatch(deleteFacilities({
-      id: selectRows[0],
-      success: () => {
-        setSelectRows([]);
-        dialogOpenDeleteCloseFn();
-      },
-    }));
+    dispatch(
+      deleteFacilities({
+        id: selectRows[0],
+        success: () => {
+          setSelectRows([]);
+          dialogOpenDeleteCloseFn();
+        },
+      })
+    );
   };
+  const handleOnSelect = (row, isSelect) => {
+    if (isSelect) {
+      const current = [...selectRows];
+      console.log(row);
+      current.push(row.id);
+      setSelectRows(current);
+    } else {
+      const current = selectRows.filter((rowValue) => rowValue != row.id);
+      setSelectRows(current);
+    }
+  };
+ const handleOnSelectAll = (isSelect, rows) => {
+    const ids = rows.map((r) => r.id);
+    if (isSelect) {
+      setSelectRows(ids);
+    } else {
+      setSelectRows([]);
+    }
+  };
+  const selectRow = {
+    mode: "checkbox",
+    clickToSelect: true,
+    selected: selectRows,
+    onSelect: handleOnSelect,
+    onSelectAll: handleOnSelectAll,
+  };
+
   return (
     <Fragment>
       <Row>
         <Col xs="12">
           <MDBCard narrow>
-            <MDBCardHeader className="view view-cascade gradient-card-header blue-gradient d-flex justify-content-between align-items-center py-2 mx-4 mt-2">
-              <div></div>
-              <div>
-                <MDBBtn onClick={dialogOpenFn}>
-                  <i className="fas fa-plus mt-0 mr-1"></i> Add Facility
-                </MDBBtn>
-                <MDBBtn
-                  disabled={selectRows.length == 0 || selectRows.length > 1}
-                  onClick={dialogOpenEditFn}
-                >
-                  <i className="fas fa-pencil-alt mt-0 mr-1"></i>Edit Facility
-                </MDBBtn>
-                <MDBBtn
-                  disabled={selectRows.length == 0 || selectRows.length > 1}
-                  onClick={dialogOpenDeleteFn}
-                >
-                  <i className="fas fa-times mt-0 mr-1"></i>Delete Facility
-                </MDBBtn>
-              </div>
-            </MDBCardHeader>
             <MDBCardBody>
-              <FacilityTable
-                facilities={{ ...tableData, rows: tableRows }}
-                editFn={dialogCloseFn}
-              />
+              <ToolkitProvider
+                keyField="id"
+                data={tableRows}
+                columns={tableData}
+                columnToggle
+                search
+              >
+                {(props) => (
+                  <div>
+                    <div className="view view-cascade gradient-card-header blue-gradient d-flex justify-content-between align-items-center py-0 mt-2 pr-0 pt-1">
+                      <ToggleList {...props.columnToggleProps} />
+                      <div>
+                        <div>
+                          <MDBBtn onClick={dialogOpenFn}>
+                            <i className="fas fa-plus mt-0 mr-1"></i> Add
+                            Facility
+                          </MDBBtn>
+                          <MDBBtn
+                            disabled={
+                              selectRows.length == 0 || selectRows.length > 1
+                            }
+                            onClick={dialogOpenEditFn}
+                          >
+                            <i className="fas fa-pencil-alt mt-0 mr-1"></i>Edit
+                            Facility
+                          </MDBBtn>
+                          <MDBBtn
+                            disabled={
+                              selectRows.length == 0 || selectRows.length > 1
+                            }
+                            onClick={dialogOpenDeleteFn}
+                          >
+                            <i className="fas fa-times mt-0 mr-1"></i>Delete
+                            Facility
+                          </MDBBtn>
+                          <SearchBar {...props.searchProps} />
+                        </div>
+                      </div>
+                    </div>
+                    <BootstrapTable
+                      {...props.baseProps}
+                      pagination={paginationFactory()}
+                      selectRow={selectRow}
+                      filter={filterFactory()}
+                    />
+                  </div>
+                )}
+              </ToolkitProvider>
             </MDBCardBody>
           </MDBCard>
         </Col>
       </Row>
       {!!isOpen && (
-        <Modal show={isOpen} close={dialogCloseFn} title={editForm.edit?"Edit Facility":"Add Facility"}>
+        <Modal
+          show={isOpen}
+          close={dialogCloseFn}
+          title={editForm.edit ? "Edit Facility" : "Add Facility"}
+        >
           <FacilityForm
-            FormModal={editForm.edit?editForm.formData:FormModal}
+            FormModal={editForm.edit ? editForm.formData : FormModal}
             close={dialogCloseFn}
             companySubmit={createFacilities}
           />
