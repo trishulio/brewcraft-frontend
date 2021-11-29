9bb970e54ba8685a70465456b9c46d7573b1f742
WaelRabah <Wael.Rabah@insat.u-carthage.tn>
2021-11-29 19:53:00 +0100

    code refactoring

--- src/pages/SupplierContact/components/details.jsx
+++ src/pages/SupplierContact/components/details.jsx
@@ -23,7 +23,7 @@ import {
     CardBody,
     CardHeader
 } from "../../../component/Common/Card";
-
+import { formatPhoneNumber, validateEmail, validatePhoneNumber } from "../../../helpers/utils";
 export default function SupplierContactDetails({ editable }) {
     const {
         invalidFirstName,
@@ -45,21 +45,6 @@ export default function SupplierContactDetails({ editable }) {
     });
 
     const dispatch = useDispatch();
-    const validatePhoneNumber=(code)=>{
-        return (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.exec(code) ? true : false)
-    }
-    const validateEmail=(email)=>{
-        return (/\S+@\S+\.\S+/.exec(email) ? true : false)
-    }
-    function formatPhoneNumber(phoneNumberString) {
-        var cleaned = ('' + phoneNumberString).replace(/\D/g, '');
-        var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
-        if (match) {
-          var intlCode = (match[1] ? '+1 ' : '');
-          return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
-        }
-        return null;
-      }
     function onFormInputChange(e) {
         switch(e.target.name) {
             case "contactFirstName":
