45570049690af226fec990cabea421751a2fd173
WaelRabah <Wael.Rabah@insat.u-carthage.tn>
2021-12-10 22:09:59 +0100

    refactoring

--- src/pages/Suppliers/components/table.jsx
+++ src/pages/Suppliers/components/table.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Redirect, useHistory } from 'react-router-dom';
+import {  useHistory } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import { useQuery } from "../../../helpers/utils";
 import { formatAddress } from "../../../helpers/textUtils";
