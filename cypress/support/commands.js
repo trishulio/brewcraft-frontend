a3a4667ac24f06f05908de5ff01d18594e399db0
guptanuj <anujgupta3821@gmail.com>
2021-12-15 22:29:14 +0530

    add cypress

---
+++ cypress/support/commands.js
@@ -0,0 +1,53 @@
+const Auth = require("aws-amplify").Auth;
+
+import "cypress-localstorage-commands";
+
+const username = Cypress.env("username");
+const password = Cypress.env("password");
+const userPoolId = Cypress.env("userPoolId");
+const clientId = Cypress.env("clientId");
+
+// const awsconfig = {
+//   aws_user_pools_id: userPoolId,
+//   aws_user_pools_web_client_id: clientId,
+// };
+
+Auth.configure({
+  region: process.env.REACT_APP_USER_POOL_REGION,
+  userPoolId: process.env.REACT_APP_USER_POOL_ID,
+  userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
+  oauth: {
+      domain: process.env.REACT_APP_USER_POOL_DOMAIN,
+      scope: [
+          "phone",
+          "email",
+          "profile",
+          "openid",
+          "aws.cognito.signin.user.admin"
+      ],
+      redirectSignIn: window.location.origin + "/",
+      redirectSignOut: window.location.origin + "/",
+      responseType: 'code',
+  }
+});
+
+
+// Auth.configure(awsconfig);
+
+Cypress.Commands.add("signIn", () => {
+  cy.then(() => Auth.signIn(username, password)).then((cognitoUser) => {
+    const idToken = cognitoUser.signInUserSession.idToken.jwtToken;
+    const accessToken = cognitoUser.signInUserSession.accessToken.jwtToken;
+
+    const makeKey = (name) =>
+      `CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${cognitoUser.username}.${name}`;
+
+    cy.setLocalStorage(makeKey("accessToken"), accessToken);
+    cy.setLocalStorage(makeKey("idToken"), idToken);
+    cy.setLocalStorage(
+      `CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.LastAuthUser`,
+      cognitoUser.username
+    );
+  });
+  cy.saveLocalStorage();
+});
