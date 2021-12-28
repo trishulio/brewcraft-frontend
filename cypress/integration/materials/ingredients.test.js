cc6f155944e3666908b74981c63728dba7448c36
WaelRabah <Wael.Rabah@insat.u-carthage.tn>
2021-12-28 22:07:26 +0100

    conflicts

--- cypress/integration/materials/ingredients.test.js
+++ cypress/integration/materials/ingredients.test.js
@@ -1,123 +1,85 @@
-/// <reference types="cypress" />
-describe("ingredients", () => {
+const uuid = () => Cypress._.random(0, 1e6);
+const id = uuid();
+
+describe("Ingredient integration test", () => {
     before(() => {
         cy.signIn();
     });
 
-    after(() => {
-        cy.clearLocalStorageSnapshot();
-        cy.clearLocalStorage();
-    });
+    it("validates normal CRUD workflow", () => {
+        const name = "ingredient-" + id;
+        cy.visit("/materials/ingredients");
+        cy.intercept("/api/v1/materials/categories?*").as("categories");
+        cy.intercept(/\/api\/v1\/materials\/\d+$/).as("fetchIngredient");
+        cy.intercept("POST", "/api/v1/materials").as("materialCreated");
+        cy.intercept("PATCH", /\/api\/v1\/materials\/\d+$/).as("materialEdit");
+        cy.intercept("DELETE", /\/api\/v1\/materials\/\d+$/).as(
+            "ingredientDelete"
+        );
 
-    beforeEach(() => {
-        cy.restoreLocalStorage();
-    });
+        /* Create an Ingredient */
 
-    afterEach(() => {
-        cy.saveLocalStorage();
-    });
-
-    it("Create an Ingredient", () => {
-        cy.fixture("ingredient").then((ingredientsJson) => {
-            cy.intercept("/api/v1/materials/categories?**").as("categories");
-            cy.intercept("POST", "/api/v1/materials").as("materialCreated");
-            cy.visit("/materials/ingredients");
-            cy.get("[data-testid=newIngredient]").should("exist").click();
-            cy.wait("@categories").then(({ response }) => {
-                expect(response.statusCode).to.eq(200);
-                expect(response.body).to.not.be.null;
-                expect(response.body.content).to.have.length.of.at.least(1);
-            });
-            cy.get("[data-testid=ingredient-name]")
-                .should("exist")
-                .type(
-                    "{selectall}{backspace}{selectall}{backspace}" +
-                        ingredientsJson.IngredientsName
-                )
-                .should("have.value", ingredientsJson.IngredientsName);
-            cy.get("[data-testid=ingredient-category]")
-                .should("exist")
-                .select("5");
-            cy.get("[data-testid=ingredient-measure]")
-                .should("exist")
-                .select("kg");
-            cy.get("[data-testid=ingredient-upc]").should("exist").type("T123");
-            cy.get("[data-testid=ingredient-description]")
-                .should("exist")
-                .type("description is coming from test!");
-            cy.get("[data-testid=ingredient-save]").should("exist").click();
-            cy.wait("@materialCreated").then(({ response }) => {
-                expect(response.statusCode).to.eq(201);
-            });
+        cy.get("[data-testid=newIngredient]").should("exist").click();
+        cy.wait("@categories").then(({ response }) => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.body.content).to.have.length.of.at.least(1);
         });
-    });
-
-    it("Edit/delete an Ingredient", () => {
-        cy.fixture("ingredient").then((ingredientsJson) => {
-            cy.intercept("/api/v1/materials/**").as("ingredientLoad");
-            cy.intercept("DELETE", "/api/v1/materials/**").as(
-                "ingredientDelete"
-            );
-            cy.intercept("/api/v1/materials/categories?**").as("categories");
-            cy.intercept("PATCH", "/api/v1/materials/**").as("materialEdit");
-
-            cy.visit("/materials/ingredients");
-            // cy.contains(ingredientsJson.IngredientsName).click();
-            cy.wait("@ingredientLoad").then(({ response }) => {
-                expect(response.statusCode).to.eq(200);
-            });
-            cy.get("[data-testid=paginationLink]").each((e) => {
-                if (!e.hasClass("active")) {
-                    e.click();
-                    cy.wait("@categories").then(({ response }) => {
-                        expect(response.statusCode).to.eq(200);
-                        expect(response.body).to.not.be.null;
-                        expect(
-                            response.body.content
-                        ).to.have.length.of.at.least(1);
-                    });
-                    cy.contains("td", ingredientsJson.IngredientsName).click();
-                    return false;
-                }
-            });
-
-            cy.wait("@ingredientLoad").then(({ response }) => {
-                expect(response.statusCode).to.eq(200);
-            });
-            cy.get("[data-testid=ingredient-edit]").click();
-            cy.get("[data-testid=ingredient-name]").type("Hi!").clear();
-            cy.focused().clear();
-            cy.wait(500).focused().type(ingredientsJson.IngredientsNameEdit);
-            cy.get("[data-testid=ingredient-save]").should("exist").click();
-            cy.wait("@materialEdit").then(({ response }) => {
-                expect(response.statusCode).to.eq(200);
-            });
-
-            cy.visit("/materials/ingredients");
-            cy.get("[data-testid=paginationLink]").each((e) => {
-                if (!e.hasClass("active")) {
-                    e.click();
-                    cy.wait("@categories").then(({ response }) => {
-                        expect(response.statusCode).to.eq(200);
-                        expect(response.body).to.not.be.null;
-                        expect(
-                            response.body.content
-                        ).to.have.length.of.at.least(1);
-                    });
-                    cy.contains(
-                        "td",
-                        ingredientsJson.IngredientsNameEdit
-                    ).click();
-                    return false;
-                }
-            });
-
-            cy.get("[data-testid=ingredient-edit]").click();
-            cy.get("[data-testid=ingredient-delete]").click();
-            cy.get("[data-testid=confirm]").click();
-            cy.wait("@ingredientDelete").then(({ response }) => {
-                expect(response.statusCode).to.eq(200);
-            });
+        cy.get("[data-testid=ingredient-name]")
+            .type(name)
+            .should("have.value", name);
+        cy.get("[data-testid=ingredient-category]").should("exist").select("5");
+        cy.get("[data-testid=ingredient-measure]").should("exist").select("kg");
+        cy.get("[data-testid=ingredient-upc]").should("exist").type("T123");
+        cy.get("[data-testid=ingredient-description]")
+            .should("exist")
+            .type("description is coming from test!");
+        cy.get("[data-testid=ingredient-save]").should("exist").click();
+        cy.wait("@materialCreated").then(({ response }) => {
+            expect(response.statusCode).to.equal(201);
+            expect(response.body.name).to.equal(name);
         });
+
+        /* Edit same Ingredient */
+
+        cy.visit("/materials/ingredients");
+        cy.get("table").contains("td", name).click();
+        cy.wait("@fetchIngredient").then(({ response }) => {
+            expect(response.statusCode).to.eq(200);
+        });
+        cy.get("[data-testid=ingredient-name]").should("have.value", name);
+        cy.get("[data-testid=ingredient-category]").should("have.value", "5");
+        cy.get("[data-testid=ingredient-measure]").should("have.value", "kg");
+        cy.get("[data-testid=ingredient-upc]").should("have.value", "T123");
+        cy.get("[data-testid=ingredient-description]").should(
+            "have.value",
+            "description is coming from test!"
+        );
+        cy.get("[data-testid=ingredient-edit]").click();
+        cy.get("[data-testid=ingredient-name]")
+            .click()
+            .focused()
+            .clear()
+            .type(name + " edit");
+        cy.get("[data-testid=ingredient-save]").should("exist").click();
+        cy.wait("@materialEdit").then(({ response }) => {
+            expect(response.statusCode).to.eq(200);
+        });
+
+        /* Delete same Ingredient */
+
+        cy.visit("/materials/ingredients");
+        cy.get("table")
+            .contains("td", name + " edit")
+            .click();
+        cy.get("[data-testid=ingredient-edit]").click();
+        cy.get("[data-testid=ingredient-delete]").click();
+        cy.get("[data-testid=confirm]").click();
+        cy.wait("@ingredientDelete").then(({ response }) => {
+            expect(response.statusCode).to.eq(200);
+        });
+        cy.visit("/materials/ingredients");
+        cy.get("table")
+            .contains("td", name + " edit")
+            .should("not.exist");
     });
 });
--- cypress/integration/materials/packaging-items.test.js
+++ cypress/integration/materials/packaging-items.test.js
@@ -1,119 +1,100 @@
-/// <reference types="cypress" />
-describe("Packaging", () => {
+const uuid = () => Cypress._.random(0, 1e6);
+const id = uuid();
+
+describe("Packaging integration test", () => {
     before(() => {
         cy.signIn();
     });
 
-    after(() => {
-        cy.clearLocalStorageSnapshot();
-        cy.clearLocalStorage();
-    });
+    it("Create an Packaging", function () {
+        const name = "Packaging-" + id;
+        cy.intercept("/api/v1/materials/categories?**").as("categories");
+        cy.intercept("POST", "/api/v1/materials").as("materialCreated");
+        cy.visit("/materials/packaging");
+        cy.get("[data-testid=newPackaging]").should("exist").click();
 
-    beforeEach(() => {
-        cy.restoreLocalStorage();
-    });
-
-    afterEach(() => {
-        cy.saveLocalStorage();
-    });
-
-    it("Create an Packaging", () => {
-        cy.fixture("ingredient").then((ingredientsJson) => {
-            cy.intercept("/api/v1/materials/categories?**").as("categories");
-            cy.intercept("POST", "/api/v1/materials").as("materialCreated");
-            cy.visit("/materials/packaging");
-            cy.get("[data-testid=newPackaging]").should("exist").click();
-
-            cy.wait("@categories").then(({ response }) => {
-                expect(response.statusCode).to.eq(200);
-                expect(response.body).to.not.be.null;
-                expect(response.body.content).to.have.length.of.at.least(1);
-            });
-            cy.get("[data-testid=packagingNname]")
-                .should("exist")
-                .type(
-                    "{selectall}{backspace}{selectall}{backspace}" +
-                        ingredientsJson.IngredientsName
-                )
-                .should("have.value", ingredientsJson.IngredientsName);
-            cy.get("[data-testid=packagingCategory]").should("exist").select(2);
-            cy.get("[data-testid=packagingMeasure]").should("exist").select(2);
-            cy.get("[data-testid=packagingUpc]").should("exist").type("T123");
-            cy.get("[data-testid=packagingDescription]")
-                .should("exist")
-                .type("description is coming from test!");
-            cy.get("[data-testid=packagingSave]").should("exist").click();
-            cy.wait("@materialCreated").then(({ response }) => {
-                expect(response.statusCode).to.eq(201);
-            });
+        cy.wait("@categories").then(({ response }) => {
+            expect(response.statusCode).to.eq(200);
+            expect(response.body).to.not.be.null;
+            expect(response.body.content).to.have.length.of.at.least(1);
         });
-    });
 
-    it("Edit/delete an Packaging", () => {
-        cy.fixture("ingredient").then((ingredientsJson) => {
-            cy.intercept("/api/v1/materials/**").as("loadData");
-            cy.intercept("DELETE", "/api/v1/materials/**").as(
-                "packagingDelete"
-            );
-            cy.intercept("/api/v1/materials/categories?**").as("categories");
-            cy.intercept("PATCH", "/api/v1/materials/**").as("materialEdit");
+        /* Create an Packaging */
 
-            cy.visit("/materials/packaging");
-            cy.wait("@loadData").then(({ response }) => {
-                expect(response.statusCode).to.eq(200);
-            });
-            cy.get("[data-testid=paginationLink]").each((e) => {
-                if (!e.hasClass("active")) {
-                    e.click();
-                    cy.wait("@categories").then(({ response }) => {
-                        expect(response.statusCode).to.eq(200);
-                        expect(response.body).to.not.be.null;
-                        expect(
-                            response.body.content
-                        ).to.have.length.of.at.least(1);
-                    });
-                    cy.contains("td", ingredientsJson.IngredientsName).click();
-                    return false;
-                }
-            });
+        cy.get("[data-testid=packagingNname]")
+            .should("exist")
+            .type("{selectall}{backspace}{selectall}{backspace}" + name)
+            .should("have.value", name);
+        cy.get("[data-testid=packagingCategory]").should("exist").select(2);
+        cy.get("[data-testid=packagingMeasure]").should("exist").select(2);
+        cy.get("[data-testid=packagingUpc]").should("exist").type("T123");
+        cy.get("[data-testid=packagingDescription]")
+            .should("exist")
+            .type("description is coming from test!");
+        cy.get("[data-testid=packagingSave]").should("exist").click();
+        cy.wait("@materialCreated").then(({ response }) => {
+            expect(response.statusCode).to.eq(201);
+        });
 
-            cy.wait("@loadData").then(({ response }) => {
-                expect(response.statusCode).to.eq(200);
-            });
-            cy.get("[data-testid=packagingEdit]").click();
-            cy.get("[data-testid=packagingNname]").type("Hi!").clear();
-            cy.focused().clear();
-            cy.wait(500).focused().type(ingredientsJson.IngredientsNameEdit);
-            cy.get("[data-testid=packagingSave]").should("exist").click();
-            cy.wait("@materialEdit").then(({ response }) => {
-                expect(response.statusCode).to.eq(200);
-            });
+        /* Edit same Packaging */
 
-            cy.visit("/materials/packaging");
-            cy.get("[data-testid=paginationLink]").each((e) => {
-                if (!e.hasClass("active")) {
-                    e.click();
-                    cy.wait("@categories").then(({ response }) => {
-                        expect(response.statusCode).to.eq(200);
-                        expect(response.body).to.not.be.null;
-                        expect(
-                            response.body.content
-                        ).to.have.length.of.at.least(1);
-                    });
-                    cy.contains(
-                        "td",
-                        ingredientsJson.IngredientsNameEdit
-                    ).click();
-                    return false;
-                }
-            });
+        cy.intercept("/api/v1/materials/**").as("loadData");
+        cy.intercept("DELETE", "/api/v1/materials/**").as("packagingDelete");
+        cy.intercept("/api/v1/materials/categories?**").as("categories");
+        cy.intercept("PATCH", "/api/v1/materials/**").as("materialEdit");
 
-            cy.get("[data-testid=packagingEdit]").click();
-            cy.get("[data-testid=packagingDelete]").click();
-            cy.get("[data-testid=confirm]").click();
-            cy.wait("@packagingDelete").then(({ response }) => {
-                expect(response.statusCode).to.eq(200);
-            });
+        cy.visit("/materials/packaging");
+        cy.wait("@loadData").then(({ response }) => {
+            expect(response.statusCode).to.eq(200);
+        });
+        cy.get("[data-testid=paginationLink]").each(function (e) {
+            if (!e.hasClass("active")) {
+                e.click();
+                cy.wait("@categories").then(({ response }) => {
+                    expect(response.statusCode).to.eq(200);
+                    expect(response.body).to.not.be.null;
+                    expect(response.body.content).to.have.length.of.at.least(1);
+                });
+                cy.contains("td", name).click();
+                return false;
+            }
+        });
+
+        cy.wait("@loadData").then(({ response }) => {
+            expect(response.statusCode).to.eq(200);
+        });
+        cy.get("[data-testid=packagingEdit]").click();
+        cy.get("[data-testid=packagingNname]")
+            .click()
+            .focused()
+            .clear()
+            .type(name + " edit");
+        cy.get("[data-testid=packagingSave]").should("exist").click();
+        cy.wait("@materialEdit").then(({ response }) => {
+            expect(response.statusCode).to.eq(200);
+        });
+
+        /* Delete same Packaging */
+
+        cy.visit("/materials/packaging");
+        cy.get("[data-testid=paginationLink]").each(function (e) {
+            if (!e.hasClass("active")) {
+                e.click();
+                cy.wait("@categories").then(({ response }) => {
+                    expect(response.statusCode).to.eq(200);
+                    expect(response.body).to.not.be.null;
+                    expect(response.body.content).to.have.length.of.at.least(1);
+                });
+                cy.contains("td", name + " edit").click();
+                return false;
+            }
+        });
+
+        cy.get("[data-testid=packagingEdit]").click();
+        cy.get("[data-testid=packagingDelete]").click();
+        cy.get("[data-testid=confirm]").click();
+        cy.wait("@packagingDelete").then(({ response }) => {
+            expect(response.statusCode).to.eq(200);
         });
     });
 });
