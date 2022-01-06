704820b139077f8a232bf36b47c98bc7576d3094
Matthew Erschbamer <erschbombr@gmail.com>
2022-01-05 20:17:37 -0800

    Fix lint errors

--- src/pages/FinishedGoods/components/toolbar.jsx
+++ src/pages/FinishedGoods/components/toolbar.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Button } from "reactstrap";
 import Toolbar from "../../../component/Common/toolbar";
 
 export default function FinishedGoodsToolbar() {
-    const history = useHistory();
     return <Toolbar></Toolbar>;
 }
--- src/pages/FinishedGoodsInventory/components/toolbar.jsx
+++ src/pages/FinishedGoodsInventory/components/toolbar.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-import { Button } from "reactstrap";
 import Toolbar from "../../../component/Common/toolbar";
 
 export default function FinishedGoodsInventoryToolbar() {
-    const history = useHistory();
-
     return <Toolbar></Toolbar>;
 }
