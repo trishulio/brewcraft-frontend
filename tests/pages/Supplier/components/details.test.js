65e5aa0905da24c836f838bb630dc2541b51ff7d
Michael Zbeetnoff <mzb99@protonmail.com>
2022-01-09 00:01:13 -0800

    Repo maintenance
    What
    Add unit tests to github pipeline.
    Add tests/ to eslint.
    Add tests/ to eslint.
    Update line endings to lf in prettier config.

--- tests/pages/Supplier/components/details.test.js
+++ tests/pages/Supplier/components/details.test.js
@@ -56,31 +56,32 @@ const mockDispatch = jest.fn();
 const mockGoBack = jest.fn();
 const mockPush = jest.fn();
 
-const mockHistory = ({
+const mockHistory = {
     replace: jest.fn(),
     goBack: mockGoBack,
-    push: mockPush
-})
+    push: mockPush,
+};
 
 jest.mock("react-redux", () => ({
     ...jest.requireActual("react-redux"),
     useDispatch: () => mockDispatch,
 }));
 
-jest.mock("react-router-dom", ()=> ({
+jest.mock("react-router-dom", () => ({
     ...jest.requireActual("react-router-dom"),
     useParams: jest.fn().mockReturnValue({
-        id: ""
+        id: "",
     }),
-    useHistory: () => mockHistory.mockReturnValue({
-        goBack: () => mockGoBack
-    })
-}))
+    useHistory: () =>
+        mockHistory.mockReturnValue({
+            goBack: () => mockGoBack,
+        }),
+}));
 
-jest.mock("react-router", ()=> ({
+jest.mock("react-router", () => ({
     ...jest.requireActual("react-router"),
-    useHistory: () => mockHistory
-}))
+    useHistory: () => mockHistory,
+}));
 
 describe("Supplier -> Components -> <Details>", () => {
     describe("render()", () => {
@@ -88,9 +89,7 @@ describe("Supplier -> Components -> <Details>", () => {
             const supplierDetailsComp = mount(
                 <Provider store={store}>
                     <BrowserRouter>
-                        <SupplierDetails
-                            editable={false}
-                        />
+                        <SupplierDetails editable={false} />
                     </BrowserRouter>
                 </Provider>
             );
@@ -100,246 +99,246 @@ describe("Supplier -> Components -> <Details>", () => {
             const supplierDetailsComp = mount(
                 <Provider store={store}>
                     <BrowserRouter>
-                        <SupplierDetails
-                            editable={true}
-                        />
+                        <SupplierDetails editable={true} />
                     </BrowserRouter>
                 </Provider>
             );
 
-            supplierDetailsComp.find('input[name="supplierName"]').simulate("change");
+            supplierDetailsComp
+                .find('input[name="supplierName"]')
+                .simulate("change");
 
             expect(shallowToJson(supplierDetailsComp)).toMatchSnapshot();
             expect(mockDispatch).toHaveBeenCalledWith({
-                   "payload":  {
-                     "data":  {
-                       "address":  {
-                         "addressLine1": "",
-                         "addressLine2": "",
-                         "city": "",
-                         "country": "",
-                         "id": "",
-                         "postalCode": "",
-                         "province": "",
-                       },
-                       "id": "",
-                       "name": "",
-                       "version": null,
-                     },
-                     "invalidName": true,
-                   },
-                   "type": "SET_SUPPLIER_DETAILS"
-             });
+                payload: {
+                    data: {
+                        address: {
+                            addressLine1: "",
+                            addressLine2: "",
+                            city: "",
+                            country: "",
+                            id: "",
+                            postalCode: "",
+                            province: "",
+                        },
+                        id: "",
+                        name: "",
+                        version: null,
+                    },
+                    invalidName: true,
+                },
+                type: "SET_SUPPLIER_DETAILS",
+            });
         });
         test("Dispatch supplierAddressLine1 is getting on input change", () => {
             const supplierDetailsComp = mount(
                 <Provider store={store}>
                     <BrowserRouter>
-                        <SupplierDetails
-                            editable={true}
-                        />
+                        <SupplierDetails editable={true} />
                     </BrowserRouter>
                 </Provider>
             );
 
-            supplierDetailsComp.find('input[name="supplierAddressLine1"]').simulate("change");
+            supplierDetailsComp
+                .find('input[name="supplierAddressLine1"]')
+                .simulate("change");
 
             expect(shallowToJson(supplierDetailsComp)).toMatchSnapshot();
             expect(mockDispatch).toHaveBeenCalledWith({
-                   "payload":  {
-                     "data":  {
-                       "address":  {
-                         "addressLine1": "",
-                         "addressLine2": "",
-                         "city": "",
-                         "country": "",
-                         "id": "",
-                         "postalCode": "",
-                         "province": "",
-                       },
-                       "id": "",
-                       "name": "",
-                       "version": null,
-                     },
-                     "invalidName": true,
-                   },
-                   "type": "SET_SUPPLIER_DETAILS"
-             });
+                payload: {
+                    data: {
+                        address: {
+                            addressLine1: "",
+                            addressLine2: "",
+                            city: "",
+                            country: "",
+                            id: "",
+                            postalCode: "",
+                            province: "",
+                        },
+                        id: "",
+                        name: "",
+                        version: null,
+                    },
+                    invalidName: true,
+                },
+                type: "SET_SUPPLIER_DETAILS",
+            });
         });
         test("Dispatch supplierAddressLine2 is getting on input change", () => {
             const supplierDetailsComp = mount(
                 <Provider store={store}>
                     <BrowserRouter>
-                        <SupplierDetails
-                            editable={true}
-                        />
+                        <SupplierDetails editable={true} />
                     </BrowserRouter>
                 </Provider>
             );
 
-            supplierDetailsComp.find('input[name="supplierAddressLine2"]').simulate("change");
+            supplierDetailsComp
+                .find('input[name="supplierAddressLine2"]')
+                .simulate("change");
 
             expect(shallowToJson(supplierDetailsComp)).toMatchSnapshot();
             expect(mockDispatch).toHaveBeenCalledWith({
-                   "payload":  {
-                     "data":  {
-                       "address":  {
-                         "addressLine1": "",
-                         "addressLine2": "",
-                         "city": "",
-                         "country": "",
-                         "id": "",
-                         "postalCode": "",
-                         "province": "",
-                       },
-                       "id": "",
-                       "name": "",
-                       "version": null,
-                     },
-                     "invalidName": true,
-                   },
-                   "type": "SET_SUPPLIER_DETAILS"
-             });
+                payload: {
+                    data: {
+                        address: {
+                            addressLine1: "",
+                            addressLine2: "",
+                            city: "",
+                            country: "",
+                            id: "",
+                            postalCode: "",
+                            province: "",
+                        },
+                        id: "",
+                        name: "",
+                        version: null,
+                    },
+                    invalidName: true,
+                },
+                type: "SET_SUPPLIER_DETAILS",
+            });
         });
         test("Dispatch supplierAddressCity is getting on input change", () => {
             const supplierDetailsComp = mount(
                 <Provider store={store}>
                     <BrowserRouter>
-                        <SupplierDetails
-                            editable={true}
-                        />
+                        <SupplierDetails editable={true} />
                     </BrowserRouter>
                 </Provider>
             );
 
-            supplierDetailsComp.find('input[name="supplierAddressCity"]').simulate("change");
+            supplierDetailsComp
+                .find('input[name="supplierAddressCity"]')
+                .simulate("change");
 
             expect(shallowToJson(supplierDetailsComp)).toMatchSnapshot();
             expect(mockDispatch).toHaveBeenCalledWith({
-                   "payload":  {
-                     "data":  {
-                       "address":  {
-                         "addressLine1": "",
-                         "addressLine2": "",
-                         "city": "",
-                         "country": "",
-                         "id": "",
-                         "postalCode": "",
-                         "province": "",
-                       },
-                       "id": "",
-                       "name": "",
-                       "version": null,
-                     },
-                     "invalidName": true,
-                   },
-                   "type": "SET_SUPPLIER_DETAILS"
-             });
+                payload: {
+                    data: {
+                        address: {
+                            addressLine1: "",
+                            addressLine2: "",
+                            city: "",
+                            country: "",
+                            id: "",
+                            postalCode: "",
+                            province: "",
+                        },
+                        id: "",
+                        name: "",
+                        version: null,
+                    },
+                    invalidName: true,
+                },
+                type: "SET_SUPPLIER_DETAILS",
+            });
         });
         test("Dispatch supplierAddressProvince is getting on input change", () => {
             const supplierDetailsComp = mount(
                 <Provider store={store}>
                     <BrowserRouter>
-                        <SupplierDetails
-                            editable={true}
-                        />
+                        <SupplierDetails editable={true} />
                     </BrowserRouter>
                 </Provider>
             );
 
-            supplierDetailsComp.find('input[name="supplierAddressProvince"]').simulate("change");
+            supplierDetailsComp
+                .find('input[name="supplierAddressProvince"]')
+                .simulate("change");
 
             expect(shallowToJson(supplierDetailsComp)).toMatchSnapshot();
             expect(mockDispatch).toHaveBeenCalledWith({
-                   "payload":  {
-                     "data":  {
-                       "address":  {
-                         "addressLine1": "",
-                         "addressLine2": "",
-                         "city": "",
-                         "country": "",
-                         "id": "",
-                         "postalCode": "",
-                         "province": "",
-                       },
-                       "id": "",
-                       "name": "",
-                       "version": null,
-                     },
-                     "invalidName": true,
-                   },
-                   "type": "SET_SUPPLIER_DETAILS"
-             });
+                payload: {
+                    data: {
+                        address: {
+                            addressLine1: "",
+                            addressLine2: "",
+                            city: "",
+                            country: "",
+                            id: "",
+                            postalCode: "",
+                            province: "",
+                        },
+                        id: "",
+                        name: "",
+                        version: null,
+                    },
+                    invalidName: true,
+                },
+                type: "SET_SUPPLIER_DETAILS",
+            });
         });
         test("Dispatch supplierAddressPostalCode is getting on input change", () => {
             const supplierDetailsComp = mount(
                 <Provider store={store}>
                     <BrowserRouter>
-                        <SupplierDetails
-                            editable={true}
-                        />
+                        <SupplierDetails editable={true} />
                     </BrowserRouter>
                 </Provider>
             );
 
-            supplierDetailsComp.find('input[name="supplierAddressPostalCode"]').simulate("change");
+            supplierDetailsComp
+                .find('input[name="supplierAddressPostalCode"]')
+                .simulate("change");
 
             expect(shallowToJson(supplierDetailsComp)).toMatchSnapshot();
             expect(mockDispatch).toHaveBeenCalledWith({
-                   "payload":  {
-                     "data":  {
-                       "address":  {
-                         "addressLine1": "",
-                         "addressLine2": "",
-                         "city": "",
-                         "country": "",
-                         "id": "",
-                         "postalCode": "",
-                         "province": "",
-                       },
-                       "id": "",
-                       "name": "",
-                       "version": null,
-                     },
-                     "invalidName": true,
-                   },
-                   "type": "SET_SUPPLIER_DETAILS"
-             });
+                payload: {
+                    data: {
+                        address: {
+                            addressLine1: "",
+                            addressLine2: "",
+                            city: "",
+                            country: "",
+                            id: "",
+                            postalCode: "",
+                            province: "",
+                        },
+                        id: "",
+                        name: "",
+                        version: null,
+                    },
+                    invalidName: true,
+                },
+                type: "SET_SUPPLIER_DETAILS",
+            });
         });
         test("Dispatch supplierAddressCountry is getting on input change", () => {
             const supplierDetailsComp = mount(
                 <Provider store={store}>
                     <BrowserRouter>
-                        <SupplierDetails
-                            editable={true}
-                        />
+                        <SupplierDetails editable={true} />
                     </BrowserRouter>
                 </Provider>
             );
 
-            supplierDetailsComp.find('input[name="supplierAddressCountry"]').simulate("change");
+            supplierDetailsComp
+                .find('input[name="supplierAddressCountry"]')
+                .simulate("change");
 
             expect(shallowToJson(supplierDetailsComp)).toMatchSnapshot();
             expect(mockDispatch).toHaveBeenCalledWith({
-                   "payload":  {
-                     "data":  {
-                       "address":  {
-                         "addressLine1": "",
-                         "addressLine2": "",
-                         "city": "",
-                         "country": "",
-                         "id": "",
-                         "postalCode": "",
-                         "province": "",
-                       },
-                       "id": "",
-                       "name": "",
-                       "version": null,
-                     },
-                     "invalidName": true,
-                   },
-                   "type": "SET_SUPPLIER_DETAILS"
-             });
+                payload: {
+                    data: {
+                        address: {
+                            addressLine1: "",
+                            addressLine2: "",
+                            city: "",
+                            country: "",
+                            id: "",
+                            postalCode: "",
+                            province: "",
+                        },
+                        id: "",
+                        name: "",
+                        version: null,
+                    },
+                    invalidName: true,
+                },
+                type: "SET_SUPPLIER_DETAILS",
+            });
         });
     });
 });
