91ba43afe844d0ecedee2f64ae0d71b0a01b6627
Michael Zbeetnoff <mzb99@protonmail.com>
2022-08-09 23:15:29 -0700

    fix mobile styling for brew page

--- src/pages/Brew/components/toolbar.jsx
+++ src/pages/Brew/components/toolbar.jsx
@@ -28,80 +28,85 @@ export default function Toolbar({ onDelete }) {
 
     const renderBatchToolbar = () => (
         <React.Fragment>
-            <Select
-                className="d-inline-block align-middle mr-3"
-                isMulti={false}
-                name="brewMonitorId"
-                value={
-                    batch.id && {
-                        id,
-                        label: `Brew ${batch.id} - ${batch.product.name}`,
+            <div
+                style={{ gap: "0.75rem 0" }}
+                className="d-flex align-items-center flex-wrap"
+            >
+                <Select
+                    className="d-inline-block align-middle mr-2"
+                    isMulti={false}
+                    name="brewMonitorId"
+                    value={
+                        batch.id && {
+                            id,
+                            label: `Brew ${batch.id} - ${batch.product.name}`,
+                        }
                     }
-                }
-                placeholder="Select Batch .."
-                options={batches.map((b) => ({
-                    value: b.id,
-                    label: `Brew ${b.id} - ${b.product.name}`,
-                }))}
-                onChange={(e) => {
-                    history.push({
-                        pathname: "/brews/" + e.value,
-                        search: "?edit=true",
-                    });
-                }}
-                styles={{
-                    control: (styles) => ({
-                        ...styles,
-                        width: "12rem",
-                        "@media screen and (max-width: 750px)": {
-                            width: "100%",
-                        },
-                    }),
-                }}
-                enabled={batch.id}
-            />
-            <Button
-                type="button"
-                color="secondary"
-                className="waves-effect d-inline align-middle mr-2"
-            >
-                <i className="fa fa-print"></i> Print
-            </Button>
-            <Button
-                type="button"
-                color="secondary"
-                className="waves-effect d-inline align-middle mr-2"
-                onClick={() => {
-                    setShowInitStage(true);
-                }}
-            >
-                <i className="fa fa-plus"></i> Add Turn
-            </Button>
-            <Button
-                type="button"
-                color="danger"
-                className="waves-effect d-inline align-middle mr-2"
-                onClick={onDelete}
-            >
-                <i className="fa fa-minus-circle"></i> Delete Brew
-            </Button>
-            <StageInitModal
-                show={showInitStage}
-                setShow={setShowInitStage}
-                title="Initial stage details"
-                addBatchStage={(equipmentItem) => {
-                    dispatch(
-                        addBatchStage({
-                            parentMixtureIds: [],
-                            taskId: 1,
-                            statusId: 4,
-                            equipment: {
-                                ...equipmentItem,
+                    placeholder="Select Batch .."
+                    options={batches.map((b) => ({
+                        value: b.id,
+                        label: `Brew ${b.id} - ${b.product.name}`,
+                    }))}
+                    onChange={(e) => {
+                        history.push({
+                            pathname: "/brews/" + e.value,
+                            search: "?edit=true",
+                        });
+                    }}
+                    styles={{
+                        control: (styles) => ({
+                            ...styles,
+                            width: "12rem",
+                            "@media screen and (max-width: 750px)": {
+                                width: "100%",
                             },
-                        })
-                    );
-                }}
-            />
+                        }),
+                    }}
+                    enabled={batch.id}
+                />
+                <Button
+                    type="button"
+                    color="secondary"
+                    className="waves-effect d-inline align-middle mr-2"
+                >
+                    <i className="fa fa-print"></i> Print
+                </Button>
+                <Button
+                    type="button"
+                    color="secondary"
+                    className="waves-effect d-inline align-middle mr-2"
+                    onClick={() => {
+                        setShowInitStage(true);
+                    }}
+                >
+                    <i className="fa fa-plus"></i> Add Turn
+                </Button>
+                <Button
+                    type="button"
+                    color="danger"
+                    className="waves-effect d-inline align-middle mr-2"
+                    onClick={onDelete}
+                >
+                    <i className="fa fa-minus-circle"></i> Delete Brew
+                </Button>
+                <StageInitModal
+                    show={showInitStage}
+                    setShow={setShowInitStage}
+                    title="Initial stage details"
+                    addBatchStage={(equipmentItem) => {
+                        dispatch(
+                            addBatchStage({
+                                parentMixtureIds: [],
+                                taskId: 1,
+                                statusId: 4,
+                                equipment: {
+                                    ...equipmentItem,
+                                },
+                            })
+                        );
+                    }}
+                />
+            </div>
         </React.Fragment>
     );
 
